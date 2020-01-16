0ed5f4648483893fbcf1787167bf67561661c94c
wkoumaka <wkoumaka@users.noreply.github.com>
2020-01-16 03:11:54 -0600

    Initial commit

---
+++ Asignmentss.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+int numb1;
+int numb2;
+int sum;
+numb1=2;
+numb2=3;
+sum =numb1+numb2;
+printf("If i add %d plus %d i will get %d",numb1,numb2,sum);
+return 0;
+
+}
